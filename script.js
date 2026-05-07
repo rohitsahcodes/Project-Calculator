154a86a35e14bc1384b35157b5a8d805c7966001
rohitsahcodes <rohitsah.codes@gmail.com>
2026-05-07 15:16:13 +0545

    Files creasted and basic html/css/js written

---
+++ script.js
@@ -0,0 +1,42 @@
+//Project-Calculator
+
+let operandFirst = 0;
+let operandSecond = 0;
+let operator = "";
+
+//funct to add two numbers
+function add(){
+
+}
+
+//function to subtract two numbers
+function subtract(){
+
+}
+
+//function to multiply two numberts
+function multiply(){
+
+}
+
+//function to divide two numbers
+function divide(){
+
+}
+
+//function to operate one by one 
+function operate(operandFirst, operator, operandSecond){
+
+
+
+}
+
+let digits = "1234567890";
+let digitArr = "";
+function checkDigit(digit){
+    digits.includes((digit));
+
+}
+let operator = "+-*/";
+let equalsTo = "=";
+let clear = "buttonClear";
